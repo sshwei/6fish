1656ad602cf71186049deb936f861196e86fa687
sshqxsz <128020119+sshwei@users.noreply.github.com>
2024-03-21 16:23:57 +0800

    Update Expension_prefix.py

--- Expension_prefix.py
+++ Expension_prefix.py
@@ -11,12 +11,6 @@ def compress_prefix(prefix):
     return compressed_prefix
 
 
-def add_random_zeros_and_ones(binary_address, prefix_length,length):
-    zeros_and_ones = ''.join(random.choice('01')for _ in range (length))
-    result = binary_address[:prefix_length] + zeros_and_ones
-    return result
-
-
 def binary_to_ipv6_prefix(binary_str):
     prefix_length = len(binary_str)
     binary_str = binary_str.ljust(128, '0')
@@ -26,35 +20,71 @@ def binary_to_ipv6_prefix(binary_str):
     return str(ipv6_prefix)
 
 
-def g_prefix(ip, prefix_length,n,cor):
+def g_prefix(ip, prefix_length,n, bounds):
     binary_address = format(int(ip), '0128b')
-    expend_pre=[]
-    length=0
+    expend_pre = []
+    length = 0
     if prefix_length is not None:
         prefix_length = int(prefix_length)
-        for cores in cor:
-            if prefix_length <=cores:
-                if cores-prefix_length>=n:
-                    length=n
-                else:
-                    length=cores-prefix_length
-                try:
-                    for _ in range(2**length):
-                        binary_address = add_random_zeros_and_ones(binary_address, prefix_length, length)
-                        hexp = binary_to_ipv6_prefix(binary_address)
-                        expend_pre.append(hexp)
-                except TypeError as e:
-                    print ("TypeError occurred:", e)
-            else:
-                length=0
-                for _ in range(2**length):
-                        binary_address = add_random_zeros_and_ones(binary_address, prefix_length, length)
-                        hexp = binary_to_ipv6_prefix(binary_address)
-                        expend_pre.append(hexp)        
+        if prefix_length>0 and prefix_length<64:
+            if bounds is not None:
+                for bound in bounds:
+                    diff=bound-prefix_length
+                    if diff==0:
+                        if n+prefix_length<=64:
+                            for i in range(2 ** n):
+                                zeros_and_ones = format(i, f'0{n}b')
+                                binary_address = binary_address[:prefix_length] + zeros_and_ones
+                                hexp = binary_to_ipv6_prefix(binary_address)
+                                expend_pre.append(hexp)
+                            break
+                        else:
+                            n=64-prefix_length
+                            for i in range(2 ** n):
+                                zeros_and_ones = format(i, f'0{n}b')
+                                binary_address = binary_address[:prefix_length] + zeros_and_ones
+                                hexp = binary_to_ipv6_prefix(binary_address)
+                                expend_pre.append(hexp)
+                            break
+                    elif diff>0 and diff<=n:
+                        n= diff+n
+                        if n+prefix_length<=64:
+                            try:
+                                for i in range(2 ** n): 
+                                    zeros_and_ones = format(i, f'0{n}b')
+                                    binary_address = binary_address[:prefix_length] + zeros_and_ones
+                                    hexp = binary_to_ipv6_prefix(binary_address)
+                                    expend_pre.append(hexp)
+                            except TypeError as e:
+                                print("TypeError occurred:", e)
+                            break
+                        else:
+                            n=64-prefix_length
+                            for i in range(2 ** n):
+                                zeros_and_ones = format(i, f'0{n}b')
+                                binary_address = binary_address[:prefix_length] + zeros_and_ones
+                                hexp = binary_to_ipv6_prefix(binary_address)
+                                expend_pre.append(hexp)
+                            break
+                    elif diff>n:
+                        length=n
+                        for i in range(2 ** length):
+                            zeros_and_ones = format(i, f'0{length}b')
+                            binary_address = binary_address[:prefix_length] + zeros_and_ones
+                            hexp = binary_to_ipv6_prefix(binary_address)
+                            expend_pre.append(hexp)
+        else:
+            length=0
+            for i in range(2 ** length):
+                zeros_and_ones = format(i, f'0{length}b')
+                binary_address = binary_address[:prefix_length-1] + zeros_and_ones
+                hexp = binary_to_ipv6_prefix(binary_address)
+                expend_pre.append(hexp)
     return expend_pre
 
 
 def process_prefix(prefix, n, cor):
+    # expendedpre=[]
     pres = []
     if prefix is not None:
         address, prefix_length = prefix.split('/')
@@ -68,5 +98,6 @@ def chanpre(prefixes, n, cor):
         results = [executor.submit(process_prefix, prefix, n, cor) for prefix in prefixes]
         for future in concurrent.futures.as_completed(results):
             pres.extend(future.result())
+            print(pres)
     return list(set(pres))
 
